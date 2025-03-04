bc71fc39f942bd7c81fed3dbb36d2eb45d34ea9b
Irshad <irshadibrahim524@gmail.com>
2025-03-04 13:51:09 +0530

    Update ChatbotRepository.java

--- src/main/java/com/example/Aichatbot/Repository/ChatbotRepository.java
+++ src/main/java/com/example/Aichatbot/Repository/ChatbotRepository.java
@@ -5,6 +5,6 @@ import com.example.Aichatbot.Entity.ChatMessage;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository  // ✅ Ensure this annotation is present
+@Repository  
 public interface ChatbotRepository extends JpaRepository<ChatMessage, Long> {
 }
